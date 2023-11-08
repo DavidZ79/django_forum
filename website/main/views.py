f743e3f628306b55e89033a6a1d2c743bc5948f3
David Zhou <david.m8f.z@gmail.com>
2023-11-08 04:42:38 -0500

    finished
    title

--- website/main/views.py
+++ website/main/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User, Group
 from .models import Post
-
-# Create your views here.
 
 
 @login_required(login_url="/login")
 def home(request):
     posts = Post.objects.all()
+
     if request.method == "POST":
         post_id = request.POST.get("post-id")
-        post = Post.objects.filter(id=post_id).first()
-        if post and post.author == request.user:
-            post.delete()
+        user_id = request.POST.get("user-id")
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
     return render(request, 'main/home.html', {"posts": posts})
 
 
 @login_required(login_url="/login")
+@permission_required("main.add_post", login_url="/login", raise_exception=True)
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -29,6 +48,7 @@ def create_post(request):
             return redirect("/home")
     else:
         form = PostForm()
+
     return render(request, 'main/create_post.html', {"form": form})
 
 
@@ -41,4 +61,5 @@ def sign_up(request):
             return redirect('/home')
     else:
         form = RegisterForm()
+
     return render(request, 'registration/sign_up.html', {"form": form})
